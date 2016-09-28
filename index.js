3237613a9018f72053588ce8eb09cbed7b6106ee
Leo <leoproechel@gmail.com>
2016-09-28 13:08:29 -0400

    created basic code to make program perform basic functioning. did minimal css work.

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
+
 $(document).ready(function(){
-	for (var i = 1; i < 17; i++) {
-		for (var j = 1; j < 17; j++) {
-			$('#container').append('<div id=square'+j+i+'></div>')
+
+	gridsize=prompt('what grid size do you want?')
+
+	for (var i = 1; i < gridsize; i++) {
+
+		for (var j = 1; j < gridsize; j++) {
+			$('#container').append('<div class="column'+i+' row'+j+'"></div>')
 		};
+
+		$('.column'+i).css({'margin-left': '4'*i})
 	};
+	for (var k = 1; k < gridsize; k++) {
+		$('.row'+k).css('top', '4'*k+'px');
+	};
+
+	$('div').mouseenter(function(){
+		$(this).css({'background-color': 'orange'})
+	});
+
+	$('#newgrid').click(function(){
+		gridsize=prompt('what grid size do you want?');
+
+//this removes previous grid when button is clicked
+	$('div>div').remove()
+//this recreates grid according to new specifications
+	for (var i = 1; i < gridsize; i++) {
+
+		for (var j = 1; j < gridsize; j++) {
+			$('#container').append('<div class="column'+i+' row'+j+'"></div>')
+		};
+
+		$('.column'+i).css({'margin-left': '4'*i})
+	};
+	for (var k = 1; k < gridsize; k++) {
+		$('.row'+k).css('top', '4'*k+'px');
+	};
+
+	$('div').mouseenter(function(){
+		$(this).css({'background-color': 'orange'})
+	});
+});
+
 });
